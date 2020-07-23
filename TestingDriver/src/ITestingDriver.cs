f2cb6749755109cabb3265e65ae2b7a676c2fd0f
RSTZZZ <zachary971011@gmail.com>
2020-07-23 11:53:58 -0400

    Update ITestingDriver.cs

--- TestingDriver/src/ITestingDriver.cs
+++ TestingDriver/src/ITestingDriver.cs
@@ -85,27 +85,27 @@ namespace TestingDriver
         /// <summary>
         /// Gets the name of the testing driver.
         /// </summary>
-        public TestingDriverType Name { get; }
+        TestingDriverType Name { get; }
 
         /// <summary>
         /// Gets the url of the page the webdriver is focued on.
         /// </summary>
-        public string CurrentURL { get; }
+        string CurrentURL { get; }
 
         /// <summary>
         /// Gets or sets the loadiong spinner that appears on the website.
         /// </summary>
-        public string LoadingSpinner { get; set; }
+        string LoadingSpinner { get; set; }
 
         /// <summary>
         /// Gets or sets the error container to check if any errors are shown on the UI.
         /// </summary>
-        public string ErrorContainer { get; set; }
+        string ErrorContainer { get; set; }
 
         /// <summary>
         /// Goes back a page.
         /// </summary>
-        public void Back();
+        void Back();
 
         /// <summary>
         /// Checks for an element state.
@@ -114,7 +114,7 @@ namespace TestingDriver
         /// <param name="state"> The state of the web element to wait for. </param>
         /// <param name="jsCommand"> Any JS command to use when finding the element.</param>
         /// <returns> If the element state is as wanted.</returns>
-        public bool CheckForElementState(string xPath, ElementState state, string jsCommand = "");
+        bool CheckForElementState(string xPath, ElementState state, string jsCommand = "");
 
         /// <summary>
         /// Performs the actions of clicking the specified element. Uses Selenium binding by default.
@@ -122,60 +122,60 @@ namespace TestingDriver
         /// <param name="xPath">The xpath to find the specified element.</param>
         /// <param name="byJS"> Whether to use JS to perform the click / not. </param>
         /// <param name="jsCommand">Any js command needed.</param>
-        public void ClickElement(string xPath, bool byJS = false, string jsCommand = "");
+        void ClickElement(string xPath, bool byJS = false, string jsCommand = "");
 
         /// <summary>
         /// Closes the current window. It will quit the browser if it is the last window opened.
         /// </summary>
-        public void CloseBrowser();
+        void CloseBrowser();
 
         /// <summary>
         /// Accepts the alert provided that there is an alert.
         /// </summary>
-        public void AcceptAlert();
+        void AcceptAlert();
 
         /// <summary>
         /// Dismisses the alert provided taht there is an alert.
         /// </summary>
-        public void DismissAlert();
+        void DismissAlert();
 
         /// <summary>
         /// Executes JS command.
         /// </summary>
         /// <param name="jsCommand">command.</param>
-        public void ExecuteJS(string jsCommand);
+        void ExecuteJS(string jsCommand);
 
         /// <summary>
         /// Goes Fowards a page.
         /// </summary>
-        public void Forward();
+        void Forward();
 
         /// <summary>
         /// Gets the text inside the alert.
         /// </summary>
         /// <returns>Alert Text.</returns>
-        public string GetAlertText();
+        string GetAlertText();
 
         /// <summary>
         /// Quits the webdriver. Call this when you want the driver to be closed.
         /// </summary>
-        public void Quit();
+        void Quit();
 
         /// <summary>
         /// Maximizes the browser.
         /// </summary>
-        public void Maximize();
+        void Maximize();
 
         /// <summary>
         /// Force kill web driver.
         /// </summary>
-        public void ForceKillWebDriver();
+        void ForceKillWebDriver();
 
         /// <summary>
         /// Generates the AODA results.
         /// </summary>
         /// <param name="folderLocation"> The folder to generate AODA results in. </param>
-        public void GenerateAODAResults(string folderLocation);
+        void GenerateAODAResults(string folderLocation);
 
         /// <summary>
         /// Returns the given attribute for the element given.
@@ -184,7 +184,7 @@ namespace TestingDriver
         /// <param name="xPath">xpath to find the attribute.</param>
         /// <param name="jsCommand">any js command to use.</param>
         /// <returns>the value of the attribute.</returns>
-        public string GetElementAttribute(string attributeName, string xPath, string jsCommand = "");
+        string GetElementAttribute(string attributeName, string xPath, string jsCommand = "");
 
         /// <summary>
         /// Returns the given text for the element given.
@@ -192,13 +192,13 @@ namespace TestingDriver
         /// <param name="xPath">xpath to find the attribute.</param>
         /// <param name="jsCommand">any js command to use.</param>
         /// <returns>the value of the attribute.</returns>
-        public string GetElementText(string xPath, string jsCommand = "");
+        string GetElementText(string xPath, string jsCommand = "");
 
         /// <summary>
         /// The GetAllLinksURL.
         /// </summary>
         /// <returns>The <see cref="T:List{string}"/>.</returns>
-        public List<string> GetAllLinksURL();
+        List<string> GetAllLinksURL();
 
         /// <summary>
         /// Tells the browser to navigate to the provided url.
@@ -206,7 +206,7 @@ namespace TestingDriver
         /// <param name="url">URL for the browser to navigate to.</param>
         /// <param name="instantiateNewDriver">Instantiates a new selenium driver.</param>
         /// <returns> <code>true</code> if the navigation was successful. </returns>C:\Users\DuongCh\Projects\TestingDrivers\TestingDriver\src\ITestingDriver.cs
-        public bool NavigateToURL(string url = "", bool instantiateNewDriver = true);
+        bool NavigateToURL(string url = "", bool instantiateNewDriver = true);
 
         /// <summary>
         /// Returns whether or not the actual attribute value of the check box matches with the expected value,
@@ -217,7 +217,7 @@ namespace TestingDriver
         /// <param name="xPath">The xpath of the element.</param>
         /// <param name="jsCommand">Any js command needed.</param>
         /// <returns><code>true</code> if actual attribute value matches with the expected value.</returns>
-        public bool VerifyAttribute(string attribute, string expectedValue, string xPath, string jsCommand = "");
+        bool VerifyAttribute(string attribute, string expectedValue, string xPath, string jsCommand = "");
 
         /// <summary>
         /// Returns whether or not the element's text is the same as the expected value.
@@ -226,7 +226,7 @@ namespace TestingDriver
         /// <param name="xPath">The xpath of the element.</param>
         /// <param name="jsCommand">Any js command needed.</param>
         /// <returns><code>true</code> if it is the same.</returns>
-        public bool VerifyElementText(string expected, string xPath, string jsCommand = "");
+        bool VerifyElementText(string expected, string xPath, string jsCommand = "");
 
         /// <summary>
         /// Returns whether or not the element is selected or not.
@@ -234,7 +234,7 @@ namespace TestingDriver
         /// <param name="xPath">The xpath of the element.</param>
         /// <param name="jsCommand">Any js command needed.</param>
         /// <returns><code>true</code> the element is current selected.</returns>
-        public bool VerifyElementSelected(string xPath, string jsCommand = "");
+        bool VerifyElementSelected(string xPath, string jsCommand = "");
 
         /// <summary>
         /// Returns whether or not the drop down list contains all the given strings.
@@ -243,7 +243,7 @@ namespace TestingDriver
         /// <param name="xPath">The xpath of the element.</param>
         /// <param name="jsCommand">Any js command needed.</param>
         /// <returns><code>true</code> the drop down contains all the given strings.</returns>
-        public bool VerifyDropDownContent(List<string> expected, string xPath, string jsCommand = "");
+        bool VerifyDropDownContent(List<string> expected, string xPath, string jsCommand = "");
 
         /// <summary>
         /// Performs the action of populating a value.
@@ -251,24 +251,24 @@ namespace TestingDriver
         /// <param name="xPath"> The xpath to use to identify the element. </param>
         /// <param name="value"> The value to populate.</param>
         /// <param name="jsCommand">Any js command needed.</param>
-        public void PopulateElement(string xPath, string value, string jsCommand = "");
+        void PopulateElement(string xPath, string value, string jsCommand = "");
 
         /// <summary>
         /// Refreshes the webpage.
         /// </summary>
-        public void RefreshWebPage();
+        void RefreshWebPage();
 
         /// <summary>
         /// Method to run aoda on the current web page.
         /// </summary>
         /// <param name="providedPageTitle"> Title of the web page the user provides. </param>
-        public void RunAODA(string providedPageTitle);
+        void RunAODA(string providedPageTitle);
 
         /// <summary>
         /// The SendKeys.
         /// </summary>
         /// <param name="keystroke">The keystroke<see cref="string"/>.</param>
-        public void SendKeys(string keystroke);
+        void SendKeys(string keystroke);
 
         /// <summary>
         /// Performs the action of selecting a value in an element.
@@ -276,31 +276,31 @@ namespace TestingDriver
         /// <param name="xPath"> The xpath to use to identify the element. </param>
         /// <param name="value"> The value to select in the element.</param>
         /// <param name="jsCommand">Any js command needed.</param>
-        public void SelectValueInElement(string xPath, string value, string jsCommand = "");
+        void SelectValueInElement(string xPath, string value, string jsCommand = "");
 
         /// <summary>
         /// Sets the global timeout in seconds.
         /// </summary>
         /// <param name="seconds">maximum duration of timeout.</param>
-        public void SetTimeOutThreshold(string seconds);
+        void SetTimeOutThreshold(string seconds);
 
         /// <summary>
         /// Switches to appropriate IFrame. Use root in xpath to leave the iframe.
         /// </summary>
         /// <param name="xPath"> xPath to find the iFrame. use "root" to leave iframe.</param>
         /// <param name="jsCommand">Any js command needed.</param>
-        public void SwitchToIFrame(string xPath, string jsCommand = "");
+        void SwitchToIFrame(string xPath, string jsCommand = "");
 
         /// <summary>
         /// The SwitchToTab.
         /// </summary>
         /// <param name="tab">The tab<see cref="int"/>.</param>
-        public void SwitchToTab(int tab);
+        void SwitchToTab(int tab);
 
         /// <summary>
         /// Takes a screenshot of the browser. Screenshot will have the datestamp as its name. Year Month Date Hour Minutes Seconds (AM/PM).
         /// </summary>
-        public void TakeScreenShot();
+        void TakeScreenShot();
 
         /// <summary>
         /// Waits for an element state.
@@ -308,22 +308,22 @@ namespace TestingDriver
         /// <param name="xPath"> The xpath to find the web element. </param>
         /// <param name="state"> The state of the web element to wait for. </param>
         /// <param name="jsCommand">Any js command needed.</param>
-        public void WaitForElementState(string xPath, ElementState state, string jsCommand = "");
+        void WaitForElementState(string xPath, ElementState state, string jsCommand = "");
 
         /// <summary>
         /// Sets implicit wait timeout in seconds.
         /// </summary>
         /// <param name="seconds">Maximum timeout duration in seconds.</param>
-        public void Wait(int seconds);
+        void Wait(int seconds);
 
         /// <summary>
         /// Waits until the loading spinner disappears.
         /// </summary>
-        public void WaitForLoadingSpinner();
+        void WaitForLoadingSpinner();
 
         /// <summary>
         /// Checks if there are any errors in the error container.
         /// </summary>
-        public void CheckErrorContainer();
+        void CheckErrorContainer();
     }
 }
